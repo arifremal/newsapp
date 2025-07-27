9aaa606205842a7ad48afec467a063d8963be1d4
Ariful Islam <remalarif0130@gmail.com>
2025-07-28 00:25:06 +0600

    pagenation added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  a='arif'
+
   render() {
     return (
-      <div>Hello my first class {this.a}</div>
+      <div>
+        <NavBar/>
+        <News/>
+      
+      </div>
     )
   }
 }
